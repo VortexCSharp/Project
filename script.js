b2ab57ae7cb0eaf70757ebbcbb803862b85819bd
João Victor Braga Rocha <vegetto.green@gmail.com>
2023-08-23 13:50:17 -0300

    Visual Things

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ html.classList.toggle("luz")
 const img = document.querySelector("#profile img")
 
 if (html.classList.contains('luz')) {
-    img.src = "./Imagens/m3touri.jpg"
+    img.src = "./Imagens/m8comp.jpg"
 }
-else img.src = "./Imagens/m8comp.jpg"
+else img.src = "./Imagens/m3touri.jpg"
 }
 document.body.style.overflow = "hidden";
 document.addEventListener("wheel", function(event) {
