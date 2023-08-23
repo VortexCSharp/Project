0305790c30727b7efe04b222c20172513421e4a2
João Victor Braga Rocha <vegetto.green@gmail.com>
2023-08-23 12:47:09 -0300

    :thumbs_up: Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode () {
+const html = document.documentElement
+html.classList.toggle("luz")
+
+
+const img = document.querySelector("#profile img")
+
+if (html.classList.contains('luz')) {
+    img.src = "./Imagens/m3touri.jpg"
+}
+else img.src = "./Imagens/m8comp.jpg"
+
+
+
+}
